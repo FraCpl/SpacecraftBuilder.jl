248fa0ed08c3744fbb72361b34909d90813a0430
FraCpl <fra.capolupo@gmail.com>
2025-08-29 15:24:43 +0200

    minor change

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,15 @@ using Quaternions
 using LinearAlgebra
 
 @testset "SpacecraftBuilder.jl" begin
-    # Write your tests here.
     JG = randn(3, 3)
+    JG = JG + JG'
     m = rand()
     r = randn(3)
     R = randn(3, 3)
+    invJ = zeros(3, 3)
+    invertInertia!(invJ, JG)
     @test norm(translateInertia(JG, m, r) - (JG - m*crossMat(r)^2)) < 1e-12
     @test norm(rotateInertia(R, JG) - R*JG*R') < 1e-12
     @test norm(translateInertiaToCoM(JG, m, r) - (JG + m*crossMat(r)^2)) < 1e-12
+    @test norm(invJ - inv(JG)) < 1e-12
 end
